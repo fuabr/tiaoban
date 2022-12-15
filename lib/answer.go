31cc792d93933b5b659d3fabc685e6228d719916
fuabr <119668727+fuabr@users.noreply.github.com>
2022-12-16 07:27:32 +0800

    Delete lib directory

--- lib/answer.go
+++
@@ -1,9 +0,0 @@
-package lib
-
-type T struct {
-	Question    string      `json:"question"`
-	Answer      string      `json:"answer"`
-	WrongAnswer string      `json:"wrongAnswer"`
-	Option      string      `json:"option"`
-	Datetime    interface{} `json:"datetime"`
-}
--- lib/study_test.go
+++
@@ -1,9 +0,0 @@
-package lib
-
-import (
-	"testing"
-)
-
-func TestName1(t *testing.T) {
-
-}
